f10ecdfbb6b5c65025e11745783b5bf4327d81df
raishoemi <raishoemi@gmail.com>
2022-10-15 15:43:19 +0300

    Add gl lib

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+extern crate gl;
+extern crate sdl2;
+
 fn main() {
-    println!("Hello, world!");
+    let sdl = sdl2::init().unwrap();
+    let video_subsystem = sdl.video().unwrap();
+    let gl_attr = video_subsystem.gl_attr();
+    gl_attr.set_context_profile(sdl2::video::GLProfile::Core);
+    gl_attr.set_context_version(4, 5);
+
+    let window = video_subsystem
+        .window("Double Pendulum", 900, 700)
+        .opengl()
+        .resizable()
+        .build().unwrap();
+    let _gl_context = window.gl_create_context().unwrap();
+    let gl = gl::Gl::load_with(|s| {
+        video_subsystem.gl_get_proc_address(s) as *const std::os::raw::c_void
+    });
 }
