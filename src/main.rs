16156c481b5b5e1c405194a28405df1600f2f537
raishoemi <raishoemi@gmail.com>
2022-10-16 14:31:33 +0300

    Add viewport and color buffer to opengl wrapper

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
+use nalgebra as na;
+
 extern crate gl;
 extern crate sdl2;
+
+pub mod opengl;
 
 fn main() {
     let sdl = sdl2::init().unwrap();
@@ -12,9 +16,34 @@ fn main() {
         .window("Double Pendulum", 900, 700)
         .opengl()
         .resizable()
-        .build().unwrap();
+        .build()
+        .unwrap();
     let _gl_context = window.gl_create_context().unwrap();
     let gl = gl::Gl::load_with(|s| {
         video_subsystem.gl_get_proc_address(s) as *const std::os::raw::c_void
     });
+
+    let color_buffer = opengl::ColorBuffer::from_color(na::Vector3::new(0.3, 0.3, 0.5));
+    color_buffer.set_used(&gl);
+
+    let mut viewport = opengl::Viewport::for_window(900, 700);
+    let mut event_pump = sdl.event_pump().unwrap();
+    'main: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                sdl2::event::Event::Quit { .. } => break 'main,
+                sdl2::event::Event::Window {
+                    win_event: sdl2::event::WindowEvent::Resized(w, h),
+                    ..
+                } => {
+                    viewport.update_size(w, h);
+                    viewport.set_used(&gl);
+                }
+                _ => {}
+            }
+        }
+        color_buffer.clear(&gl);
+        // Render stuff
+        window.gl_swap_window();
+    }
 }
---
+++ src/opengl/mod.rs
@@ -0,0 +1,5 @@
+mod viewport;
+mod color_buffer;
+
+pub use self::viewport::Viewport;
+pub use self::color_buffer::ColorBuffer;
