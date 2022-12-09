a9e8e3ea564e37ab729c6a1bb2a8575f306bf509
raishoemi <raishoemi@gmail.com>
2022-12-09 13:33:51 +0200

    Format file

--- src/geometry.rs
+++ src/geometry.rs
@@ -1,55 +1,71 @@
+#[rustfmt::skip]
 pub fn cube(length: f32) -> Vec<f32> {
     let half_len: f32 = length / 2.0;
     let mut vertices = vec![];
     // front-back
     vertices.extend(vec![
-        -half_len, half_len, half_len, half_len, half_len, half_len, half_len, -half_len, half_len,
+        -half_len, half_len, half_len,
+        half_len, half_len, half_len,
+        half_len, -half_len, half_len,
     ]);
     vertices.extend(vec![
-        half_len, -half_len, half_len, -half_len, -half_len, half_len, -half_len, half_len,
-        half_len,
+        half_len, -half_len, half_len,
+        -half_len, -half_len, half_len,
+        -half_len, half_len, half_len,
     ]);
     vertices.extend(vec![
-        -half_len, half_len, -half_len, half_len, half_len, -half_len, half_len, -half_len,
-        -half_len,
+        -half_len, half_len, -half_len,
+        half_len, half_len, -half_len,
+        half_len, -half_len, -half_len,
     ]);
     vertices.extend(vec![
-        half_len, -half_len, -half_len, -half_len, -half_len, -half_len, -half_len, half_len,
-        -half_len,
+        half_len, -half_len, -half_len,
+        -half_len, -half_len, -half_len,
+        -half_len, half_len, -half_len,
     ]);
 
     // sides
     vertices.extend(vec![
-        half_len, half_len, half_len, half_len, half_len, -half_len, half_len, -half_len, half_len,
+        half_len, half_len, half_len,
+        half_len, half_len, -half_len,
+        half_len, -half_len, half_len,
     ]);
     vertices.extend(vec![
-        half_len, -half_len, -half_len, half_len, -half_len, half_len, half_len, half_len,
-        -half_len,
+        half_len, -half_len, -half_len,
+        half_len, -half_len, half_len,
+        half_len, half_len, -half_len,
     ]);
     vertices.extend(vec![
-        -half_len, half_len, half_len, -half_len, half_len, -half_len, -half_len, -half_len,
-        half_len,
+        -half_len, half_len, half_len,
+        -half_len, half_len, -half_len,
+        -half_len, -half_len, half_len,
     ]);
     vertices.extend(vec![
-        -half_len, -half_len, -half_len, -half_len, -half_len, half_len, -half_len, half_len,
-        -half_len,
+        -half_len, -half_len, -half_len,
+        -half_len, -half_len, half_len,
+        -half_len, half_len, -half_len,
     ]);
 
     // bottom-top
     vertices.extend(vec![
-        half_len, half_len, half_len, half_len, half_len, -half_len, -half_len, half_len, half_len,
+        half_len, half_len, half_len,
+        half_len, half_len, -half_len,
+        -half_len, half_len, half_len,
     ]);
     vertices.extend(vec![
-        -half_len, half_len, -half_len, half_len, half_len, -half_len, -half_len, half_len,
-        half_len,
+        -half_len, half_len, -half_len,
+        half_len, half_len, -half_len,
+        -half_len, half_len, half_len,
     ]);
     vertices.extend(vec![
-        half_len, -half_len, half_len, half_len, -half_len, -half_len, -half_len, -half_len,
-        half_len,
+        half_len, -half_len, half_len,
+        half_len, -half_len, -half_len,
+        -half_len, -half_len, half_len,
     ]);
     vertices.extend(vec![
-        -half_len, -half_len, -half_len, half_len, -half_len, -half_len, -half_len, -half_len,
-        half_len,
+        -half_len, -half_len, -half_len,
+        half_len, -half_len, -half_len,
+        -half_len, -half_len, half_len,
     ]);
     vertices
 }
